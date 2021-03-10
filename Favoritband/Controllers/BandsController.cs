3aa6c00da615a997f5c932c3b2598f257d147c94
GitSpyke <36282472+GitSpyke@users.noreply.github.com>
2021-03-10 09:42:22 +0100

    Add files via upload

---
+++ Favoritband/Controllers/BandsController.cs
@@ -0,0 +1,28 @@
+﻿using Favoritband.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Favoritband.Controllers
+{
+
+    public class BandsController : Controller
+    {
+        BandService service = new BandService();
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            var bands = service.GetAllBands();
+            return View(bands);
+        }
+        [Route("bands/{Id}")]
+        public IActionResult Description(int id)
+        {
+            var band = service.GetBandById(id);
+            return View(band);
+        }
+    }
+}
---
+++ Favoritband/Models/BandService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Favoritband.Models
+{
+    public class BandService
+    {
+        static List<Band> bands = new List<Band>
+        {
+            new Band{ Id = 1, Name = "Linkin Park", Description = "Rock", Image = "/Linkin-park.jpg" },
+            new Band{ Id = 2, Name = "Hot Chip", Description = "Pepp", Image = "/Hot-Chip.webp"},
+            new Band{ Id = 3, Name = "Beastie boys", Description = "Hip Hop", Image = "/Beastie_Boys.jpg" },
+            new Band{ Id = 4, Name = "Guns and Roses", Description = "Paradise city", Image = "/Guns-N-Roses.jpg" },
+
+        };
+
+        public Band GetBandById(int id)
+        {
+            return bands
+                .Where(o => o.Id == id)
+                .FirstOrDefault();
+                
+        }
+
+        public Band[] GetAllBands()
+        {
+            return bands
+                .OrderBy(o => o.Name)
+                .ToArray();
+
+        }
+    }
+}
